c641902ce8d34d4667f7cfc666e535d6576d17fd
mrf1n <mrf1n@github.com>
2023-06-12 11:50:35 +0200

    Initial commit

---
+++ src/main/java/com/github/mrf1n/database/NoSqlApplication.java
@@ -0,0 +1,15 @@
+package com.github.mrf1n.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App class.
+ */
+@SpringBootApplication
+public class NoSqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoSqlApplication.class, args);
+  }
+}
---
+++ src/main/java/com/github/mrf1n/database/configuration/ApplicationConfiguration.java
@@ -0,0 +1,25 @@
+package com.github.mrf1n.database.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis Configuration.
+ */
+
+@Configuration
+@EnableCaching
+public class ApplicationConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setDefaultSerializer(new StringRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
